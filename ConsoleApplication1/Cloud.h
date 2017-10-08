a8b0aa98de7c0f3e30843f2afbd2ebcbaf179f87
John <swanjohn79@gmail.com>
2017-10-09 00:58:34 +0200

    Merge Cloud.h

---
+++ ConsoleApplication1/Cloud.h
@@ -0,0 +1,74 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+//#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Cloud {
+
+public:
+	int addSensor(int, int, int, int);
+	void printUnits();
+	void prepare();
+	void menu();
+	int id_to_int();
+	//void menu(int);
+	//void test();
+
+};
+
+
+class Sensor {
+
+public:
+	int tmpCounter = 1;
+	int doorCounter = 1;
+	int irCounter = 1;
+	int counter = 3;
+	std::string name;
+	int value;
+	int status;
+	std::string id;
+	Sensor();
+	//void addSensor();
+	//void test();
+
+};
+
+class Adv_sensor : public Sensor {
+
+public:
+	std::string param;
+	int param_value;
+	Adv_sensor();
+};
+
+
+class CPanel : public Cloud {
+	int serial;
+public:
+	//Constructor
+	CPanel();
+	//Member functions
+	void printCPanel();
+	void menuCPanel();
+	void sensorParam(int);
+	void Set_Status();
+
+};
+
+extern std::vector<Sensor> sensor;
+extern std::vector<Adv_sensor> adv_sensor;
+
+
+
+// TODO: reference additional headers your program requires here
